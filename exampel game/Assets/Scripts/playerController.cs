f087fee66d2a07c4cbd24d7b174a2ce44cf5096c
mamboGit <kevan.jalili@gmail.com>
2018-02-04 20:23:30 +0100

    Adding playerControllerScript
    Adding playerControllerScript, for movements.
    Organize folders...

---
+++ exampel game/Assets/Scripts/playerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float thrust;
+    public float JumpHeight;
+    public bool isGrounded;
+    public float GravityStrength;
+
+    Rigidbody rb;
+
+
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+        Vector3 gravityS = new Vector3(0, GravityStrength, 0);
+        Physics.gravity = gravityS;
+
+        isGrounded = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("d"))
+        {
+            gameObject.transform.position = new Vector3(transform.position.x + 6 * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey("a"))
+        {
+            gameObject.transform.position = new Vector3(transform.position.x - 6 * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w") && isGrounded == true)
+
+        {
+            Jump();
+
+        }
+    }
+
+    void Jump()
+    {
+
+        rb.AddForce(new Vector3(0, JumpHeight, 0));
+        isGrounded = false;
+    }
+
+    void OnCollisionEnter()
+    {
+        isGrounded = true;
+
+    }
+
+
+
+
+
+
+
+
+}
+
