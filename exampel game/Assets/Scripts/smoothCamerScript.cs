a1547ba5639db4e198ce3ac75a92e66fb5a2ace0
mamboGit <kevan.jalili@gmail.com>
2018-02-04 21:23:01 +0100

    Add SmoothCameraScript

---
+++ exampel game/Assets/Scripts/smoothCamerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoothCamerScript : MonoBehaviour {
+
+    public Transform lookAt;
+
+    private bool smooth = true;
+    private float smoothSpeed = 0.125f;
+    private Vector3 offset = new Vector3(0, 2.42f, -10f);
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = lookAt.transform.position + offset;
+
+
+        transform.position = desiredPosition;
+    }
+
+
+}
