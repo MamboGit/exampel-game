6460ff4929caf383a7c7e6a48021d62428a21033
mamboGit <kevan.jalili@gmail.com>
2018-02-05 20:17:15 +0100

    Adding Jump function
    Adding Jump function to playerController BJ

--- exampel game/Assets/Scripts/PlayerController_BJ.cs
+++ exampel game/Assets/Scripts/PlayerController_BJ.cs
@@ -7,11 +7,19 @@ public class PlayerController_BJ : MonoBehaviour {
     public float moveSpeed;
     public Rigidbody myRigidbody;
 
-	// Use this for initialization
-	void Start ()
+    public float jumpSpeed;
+
+    public float gravityScale = 1.0f;
+    public static float globalGravity = -9.81f;
+
+    public bool isGrounded;
+
+    // Use this for initialization
+    void Start ()
     {
         myRigidbody = GetComponent<Rigidbody>();
-	}
+        isGrounded = true;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -28,7 +36,31 @@ public class PlayerController_BJ : MonoBehaviour {
         {
             myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
         }
-            
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
+            isGrounded = false;
         }
-	}
+            
+    }
+
+    void OnCollisionEnter()
+    {
+        isGrounded = true;
+
+    }
+
+     void OnEnable()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        myRigidbody.AddForce(gravity, ForceMode.Acceleration);
+    }
+}
 
